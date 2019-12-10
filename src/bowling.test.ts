4064fdaeb557b0f4af09587cc0ccc20e2e16280f
Sammy Teillet <sammy.teillet@gmail.com>
2019-12-10 15:09:35 +0100

    Clean fresh start

--- src/bowling.test.ts
+++ src/bowling.test.ts
@@ -1,12 +1,8 @@
-import  {Bowling} from './bowling'
+import { Bowling } from "./bowling";
 
-describe('Bowling', () => {
-  it('handles a very boring game', () => {
+describe("Bowling", () => {
+  it("handles a very boring game", () => {
     const bowling = new Bowling();
-
-    bowling.speak()
-
     expect(bowling.getScore()).toBe(0);
   });
-
 });
